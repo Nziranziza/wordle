5132d02e279b89b1c710d33c6617fc7ef5513733
Nziranziza <nziranzizadaniel@gmail.com>
2022-07-26 09:28:36 +0200

    Handle blacklisted keys - Color the keyboard keys on submit - If color exist in word use warning color - If the color exist and in the right position use green - else use error color

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,5 @@
 import type { FunctionComponent } from 'react';
-import { useState } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import {
   Container,
   Chip,
@@ -21,14 +21,14 @@ const theme = createTheme({
   palette: {
     error: {
       main: grey[600],
-    }
+    },
   },
 });
 
 const getRandomWord = (): string => {
   const index = Math.floor(Math.random() * dictionary.length);
   return dictionary[index];
-}
+};
 
 type SmartChipProps = {
   label: string;
@@ -43,29 +43,22 @@ const SmartChip: FunctionComponent<SmartChipProps> = ({
   submitted,
 }) => {
   let color: any = 'default';
-  if(submitted) {
+  if (submitted) {
     if (exact) {
       color = 'success';
     } else if (exist) {
       color = 'warning';
     } else {
-      color = 'error'
+      color = 'error';
     }
   }
 
-  return (
-    <Chip
-      className="chip"
-      size='medium'
-      label={label}
-      color={color}
-    />
-  );
+  return <Chip className='chip' size='medium' label={label} color={color} />;
 };
 
 type Blacklisted = {
-  [key: string]: boolean;
-}
+  [key: string]: 'warning' | 'error' | 'success';
+};
 
 export default function Index() {
   const [board, setBoard] = useState(
@@ -76,12 +69,7 @@ export default function Index() {
   const [gameOver, setGameOver] = useState<boolean>(false);
   const [isWinner, setWinner] = useState<boolean>(false);
   const [todayWord, setWord] = useState<string>(getRandomWord());
-
-  /**
-   * TODO: 
-   * 1. Handle blacklisted characters
-   * 2. Check if the word is in the dictionary
-   */
+  const [message, setMessage] = useState('');
   const [blackListed, setBlackListed] = useState<Blacklisted>({});
 
   /**
@@ -111,50 +99,75 @@ export default function Index() {
     const currentBoardRow = board[currentGuess];
     currentBoardRow[currentBoardRow.filter((c) => c).length - 1] = null;
     setBoard((board) =>
-        board.map((row, index) =>
-          index === currentGuess ? currentBoardRow : row
-        )
-      );
-  }
+      board.map((row, index) =>
+        index === currentGuess ? currentBoardRow : row
+      )
+    );
+  };
+
+  /**
+   * Check if a word is in the dictionary
+   */
+  const isWordInDictionary = (word: string) => {
+    return dictionary.includes(word);
+  };
 
   /**
    * @description: Handle enter key press
    * 1. If current guess is not the last row, move to next row
    * 2. If current guess is same as today word, set isWinner to true and set gameOver to true
-   * 2. If current guess is the last row, check if it is the last row of the board
+   * 3. If current guess is the last row, check if it is the last row of the board
    */
   const onEnter = () => {
     const currentBoardRow = board[currentGuess];
-    if(!currentBoardRow.filter((c) => !c).length) {
-      setCurrentGuess((guess) => guess + 1)
+    if (!isWordInDictionary(currentBoardRow.join())) {
+      setMessage('Word not found');
+      return;
+    }
+    if (!currentBoardRow.filter((c) => !c).length) {
+      setCurrentGuess((guess) => guess + 1);
     }
     if (currentBoardRow.join('').toLowerCase() === todayWord.toLowerCase()) {
       setGameOver(true);
       setWinner(true);
+      setMessage('You won!');
     }
-  }
+    if(currentGuess === board.length - 1) {
+      setGameOver(true);
+      setWinner(false);
+      setMessage('You lost!');
+    }
+  };
 
   /**
    * @description: This function checks if the character is contained in today word
-   * @param character 
+   * @param character
    * @returns boolean
    */
-  const doesItExist = (character: string): boolean => {
-    if (!character) return false;
-    return todayWord.toLowerCase().includes(character.toLowerCase());
-  };
+  const doesItExist = useCallback(
+    (character: string): boolean => {
+      if (!character) return false;
+      return todayWord.toLowerCase().includes(character.toLowerCase());
+    },
+    [todayWord]
+  );
 
   /**
    * @description: This function check is the character is exact match
    * To be exact match, a charact must be in the word and the position must be the same
-   * @param character 
-   * @param characterIndex 
+   * @param character
+   * @param characterIndex
    * @returns boolean
    */
-  const isExact = (character: string, characterIndex: number): boolean => {
-    if (!character) return false;
-    return character.toLowerCase() === todayWord.toLowerCase()[characterIndex];
-  };
+  const isExact = useCallback(
+    (character: string, characterIndex: number): boolean => {
+      if (!character) return false;
+      return (
+        character.toLowerCase() === todayWord.toLowerCase()[characterIndex]
+      );
+    },
+    [todayWord]
+  );
 
   /**
    * @description: This function reset game to the default state
@@ -162,18 +175,49 @@ export default function Index() {
    * 2. Reset the board
    * 3. Set game over to false
    * 4. Get a new random word
+   * 5. Set blackisted to empty object
    */
   const restart = () => {
+    onContinue();
     setWinner(false);
     setBoard(new Array(6).fill(null).map(() => new Array(5).fill(null)));
     setCurrentGuess(0);
     setGameOver(false);
     setWord(getRandomWord());
+    setBlackListed({});
+  };
+
+  useEffect(() => {
+    let keys: Blacklisted = {};
+    board.forEach((column, colIndex) => {
+      const isSubmitted = colIndex < currentGuess || colIndex === 6;
+      if (isSubmitted) {
+        column.forEach((character, characterIndex) => {
+          keys = {
+            ...keys,
+            [character]:
+              keys[character] === 'success' ||
+              isExact(character, characterIndex)
+                ? 'success'
+                : doesItExist(character)
+                ? 'warning'
+                : 'error',
+          };
+        });
+      }
+    });
+    if (Object.keys(keys).length) {
+      setBlackListed(keys);
+    }
+  }, [board, currentGuess, isExact, doesItExist]);
+
+  const onContinue = () => {
+    setMessage('');
   };
 
   return (
     <ThemeProvider theme={theme}>
-      <Container maxWidth='xs' className="title">
+      <Container maxWidth='xs' className='title'>
         <Box
           display='flex'
           flexDirection='column'
@@ -204,32 +248,31 @@ export default function Index() {
           >
             {board.map((column, colIndex) =>
               column.map((row, rowIndex) => {
-              return (
-                <Grid item key={`${colIndex}${rowIndex}`}>
-                  <SmartChip
-                    label={row}
-                    submitted={colIndex < currentGuess || colIndex === 6}
-                    exist={doesItExist(row)}
-                    exact={isExact(row, rowIndex)}
-                  />
-                </Grid>
-              )
-            })
+                return (
+                  <Grid item key={`${colIndex}${rowIndex}`}>
+                    <SmartChip
+                      label={row}
+                      submitted={colIndex < currentGuess || colIndex === 6}
+                      exist={doesItExist(row)}
+                      exact={isExact(row, rowIndex)}
+                    />
+                  </Grid>
+                );
+              })
             )}
           </Grid>
         </Box>
       </Container>
-      <Container maxWidth='sm' className="keyboard">
+      <Container maxWidth='sm' className='keyboard'>
         <Box mt={5} display='flex' gap={GAP} flexDirection='column'>
           <Box display='flex' gap={GAP}>
             {keyboard[0].split('').map((c) => (
               <Button
-                color='secondary'
+                color={blackListed[c] || 'secondary'}
                 variant='contained'
-                className="keyboard-btn"
+                className='keyboard-btn'
                 key={c}
                 onClick={() => onKeyPress(c)}
-                disabled={gameOver}
               >
                 {c}
               </Button>
@@ -238,12 +281,11 @@ export default function Index() {
           <Box display='flex' gap={GAP}>
             {keyboard[1].split('').map((c) => (
               <Button
-                color='secondary'
+                color={blackListed[c] || 'secondary'}
                 variant='contained'
-                className="keyboard-btn"
+                className='keyboard-btn'
                 key={c}
                 onClick={() => onKeyPress(c)}
-                disabled={gameOver}
               >
                 {c}
               </Button>
@@ -251,7 +293,7 @@ export default function Index() {
           </Box>
           <Box display='flex' gap={GAP}>
             <Button
-              className="keyboard-btn flex-3"
+              className='keyboard-btn flex-3'
               variant='contained'
               color='secondary'
               onClick={onBackSpace}
@@ -260,18 +302,17 @@ export default function Index() {
             </Button>
             {keyboard[2].split('').map((c) => (
               <Button
-                color='secondary'
+                color={blackListed[c] || 'secondary'}
                 variant='contained'
-                className="keyboard-btn"
+                className='keyboard-btn'
                 key={c}
                 onClick={() => onKeyPress(c)}
-                disabled={gameOver}
               >
                 {c}
               </Button>
             ))}
             <Button
-              className="keyboard-btn flex-3"
+              className='keyboard-btn flex-3'
               variant='contained'
               color='secondary'
               onClick={onEnter}
@@ -281,10 +322,14 @@ export default function Index() {
           </Box>
         </Box>
       </Container>
-      <Dialog open={isWinner}>
-        <Box p={5}>
-          <Typography align='center'>You won!</Typography>
-          <Button onClick={restart}>Restart</Button>
+      <Dialog open={Boolean(message)}>
+        <Box p={5} display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+          <Typography align='center'>{message}</Typography>
+          {gameOver || isWinner ? (
+            <Button onClick={restart}>Restart</Button>
+          ) : (
+            <Button onClick={onContinue}>Continue</Button>
+          )}
         </Box>
       </Dialog>
     </ThemeProvider>
@@ -292,5 +337,5 @@ export default function Index() {
 }
 
 export function links() {
-  return [{ rel: "stylesheet", href: styles }];
+  return [{ rel: 'stylesheet', href: styles }];
 }
